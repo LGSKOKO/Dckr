71bee7e4c562811851f38c6bc77091aaa25dbcef
LGSKOKO <420422844@qq.com>
2021-04-19 22:19:51 +0800

    自定义工具文件

---
+++ src/commonUtil.js
@@ -0,0 +1,40 @@
+import {Message,Notice} from "view-design";
+//公共类
+let commonUtil = {
+
+};
+
+//判断数组是否为空
+commonUtil.isArrayEmpty = function (arr) {
+    return arr.length <= 0;
+};
+//判断字符串是否为空
+commonUtil.isStrEmpty = function (str){
+    str = str.trim();
+    if (typeof str == null || str === "undefined" || str === "") {
+        return true;
+    }
+    //检验字符串是否都为空
+    let regex = "^[ ]+$";
+    let re = new RegExp(regex);
+    return re.test(str);
+};
+
+//深拷贝
+commonUtil.deepClone = function (obj){
+    return JSON.parse(JSON.stringify(obj));
+}
+
+commonUtil.error = function (title) {
+    Notice.error({
+        title: title,
+    });
+};
+
+commonUtil.success = function (title) {
+    Notice.success({
+        title: title,
+    });
+}
+
+export default commonUtil;
